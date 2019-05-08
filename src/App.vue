ae6779f9180c670ff2f541a097831f1f141a3df8
taggaddaaaa <scaizerg@student.42.fr>
2019-05-09 01:56:24 +0200

    Add a Product component and render it in App vue.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="container">
-    <div class="row">
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6" :key="product.id" v-for="product in products">
+                <product></product>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
-<script>
 
-export default {
-  name: 'app',
-}
+<script>
+	import products from "@/products.json";
+	import Product from '@/components/Product.vue';
+
+	export default {
+		name: 'app',
+
+		components: {
+			Product
+		},
+
+		data() {
+			return {
+				products
+			}
+		}
+	}
 </script>
 
-<style>
 
+<style>
+    body {
+        background-color: aliceblue;
+    }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="card my-5">
+    <div class="card-body">
+        <h5 class="text-center card-title">Nom du produit</h5>
+
+        <p class="text-center text-muted card-text display-4">$12</p>
+
+        <button class="btn btn-primary form-control">Ajouter au panier</button>
+    </div>
+</div>
+</template>
+
+<script>
+	export default {
+		name: "Product"
+	}
+</script>
+
+<style scoped>
+
+</style>
