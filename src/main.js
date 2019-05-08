f87bf55bc2de1dc37b3a35e24d28d2e803ddc960
taggaddaaaa <scaizerg@student.42.fr>
2019-05-09 00:30:31 +0200

    First look and understanding of the vue project structure from  vue cli install

--- src/main.js
+++ src/main.js
@@ -4,5 +4,10 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  /**
+   * Here, We render the imported Component App on the html element #app (public/index.html).
+   * Also, it compiles all the js scripts from files that are imported here
+   * and auto injected them inside 'public/index.html' file
+   */
   render: h => h(App),
 }).$mount('#app')
