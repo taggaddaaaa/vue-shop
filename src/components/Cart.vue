57baf53dda88af009e4e493e1943611ae5c9065e
taggaddaaaa <scaizerg@student.42.fr>
2019-05-11 01:13:08 +0200

    Display total amount of cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,16 +1,29 @@
 <template>
-    <ul class="list-group">
-        <li class="list-group-item" v-for="item in items" :key="item.id">
-            {{ item.title }} - ${{ item.price }}
-            <button @click="$emit('remove-from-cart', item)" class="btn badge badge-danger float-right">Remove from Cart</button>
-        </li>
-    </ul>
+    <div>
+        <div>
+            <ul class="list-group">
+                <li class="list-group-item" v-for="item in items" :key="item.id">
+                    {{ item.title }} - ${{ item.price }}
+                    <button @click="$emit('remove-from-cart', item)" class="btn badge badge-danger float-right">Remove from Cart</button>
+                </li>
+            </ul>
+        </div>
+        <div class="card p-3 my-5">
+            <h4 class="text-center">${{ total}}</h4>
+        </div>
+    </div>
+
 </template>
 
 <script>
 	export default {
 		name: 'Cart',
-		props: ['items']
+		props: ['items'],
+		computed: {
+			total() {
+				return this.items.reduce((acc, item) => acc + Number(item.price), 0);
+			}
+		}
 	}
 </script>
 
